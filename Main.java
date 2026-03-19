f5091dbcaed87c2cd5de858606531365d96442f8
b2230356069 <b2230356069@cs.hacettepe.edu.tr>
2026-03-19 20:49:36 +0300

    RadixSort has been added.

--- Main.java
+++ Main.java
@@ -175,4 +175,56 @@ public class Main {
             
         }
     }
+
+    public static int[] radixSort(int[] A, int d){
+
+        for (int pos = 1; pos <= d; pos++){
+
+            A = countingSort(A, pos);
+
+        }
+        
+        return A;
+
+    }
+
+    public static int[] countingSort(int[] A, int pos){
+
+        int[] count = new int[10];
+        int[] output = new int[A.length];
+        int size = A.length;
+
+        for (int i = 0; i < size; i++){
+
+            int digit = getDigit(A[i], pos);
+            count[digit] = count[digit] + 1;
+
+        }
+
+        for (int i = 1; i < 10; i++){
+
+            count[i] = count[i] + count[i-1];
+
+        }
+
+        for (int i = size-1; i >= 0; i--){
+
+            int digit = getDigit(A[i], pos);
+            count[digit] = count[digit] - 1;
+            output[count[digit]] = A[i];
+
+        }
+        
+        return output;
+
+    }
+
+    public static int getDigit(int num, int pos){
+
+        for (int i = 1; i < pos; i++) {
+            num = num / 10;
+        }
+        return num % 10;
+
+    }
 }
